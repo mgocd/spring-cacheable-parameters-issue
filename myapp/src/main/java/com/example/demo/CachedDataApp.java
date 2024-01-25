18d04d30a82d8a0d5fd98db6d75ce0d1c75c9d3f
mgocd <145555134+mgocd@users.noreply.github.com>
2024-01-25 09:41:53 +0100

    Update params

--- myapp/src/main/java/com/example/demo/CachedDataApp.java
+++ myapp/src/main/java/com/example/demo/CachedDataApp.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class CachedDataApp {
 
-    @Cacheable(value = "myCache", key = "{ #param1 }")
+    @Cacheable(value = "myCache", key = "{ #param1, #param2 }")
     public String get(String param1, String param2) {
         return param1 + "/" + param2;
     }
--- mylib/src/main/java/com/example/demo/mylib/CachedDataLib.java
+++ mylib/src/main/java/com/example/demo/mylib/CachedDataLib.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class CachedDataLib {
 
-    @Cacheable(value = "myCacheLib", key = "{ #param1 }")
+    @Cacheable(value = "myCacheLib", key = "{ #param1, #param2 }")
     public String get(String param1, String param2) {
         return param1 + "/" + param2;
     }
